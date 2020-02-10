ad8b96bd5b2ee204dcf62690507e55fe08042d0d
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2020-02-10 15:29:08 -0500

    Yay

---
+++ CCC_2020 Practice/src/ccc/J12019.java
@@ -0,0 +1,27 @@
+package ccc;
+
+import java.util.Scanner; 
+
+class J12019 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in); 
+        int a1 = Integer.in.nextLine(); 
+        int a2 = in.nextLine(); 
+        int a3 = in.nextLine(); 
+        
+        int b1 = in.nextLine(); 
+        int b2 = in.nextLine(); 
+        int b3 = in.nextLine(); 
+        
+        int aTotal = a1 + a2*2 + a3*3; 
+        int bTotal = b1+2*b2+3*b3; 
+        
+        if(aTotal > bTotal) 
+        	System.out.println("A");
+        else if(bTotal > aTotal) {
+        	
+        }
+        
+        
+    }
+}
